4ad101af8d4b69d10b23b22806e9d7d6fa97073b
Malick <malick.traore@efrei.net>
2026-04-16 16:35:35 +0200

    🚨 BUG: Test de rollback

--- simple-api/src/main/java/fr/takima/training/simpleapi/SimpleApiApplication.java
+++ simple-api/src/main/java/fr/takima/training/simpleapi/SimpleApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SimpleApiApplication {
 
 	public static void main(String[] args) {
+		Ceci est un bug volontaire pour le rollback;
 		SpringApplication.run(SimpleApiApplication.class, args);
 	}
 
